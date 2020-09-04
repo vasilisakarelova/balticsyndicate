bdcccfd64472b26fdcc105afaad57471f187a095
vasilisa <igorevna.vasilisa@gmail.com>
2020-09-04 10:11:48 +0200

    updates to layout

--- src/components/URLImage.js
+++ src/components/URLImage.js
@@ -6,18 +6,22 @@ import useImage from 'use-image'
 let cache = {}
 
 const URLImage = ({ data, onClick, idx }, ref) => {
-  const { x, y, slug } = data
+  const { y, slug, prevHeight } = data
   const cardRef = useRef(null)
   const [{ scale }, set] = useSpring(() => ({ scale: 1, visibility: 'visible' }))
   const [image] = useImage(data.img)
-  const [width, setWidth] = useState(400)
-  const [height, setHeight] = useState(400)
+  const [width, setWidth] = useState(300)
+  const [height, setHeight] = useState(300)
+  const [x, setX] = useState(window.innerWidth / 2)
 
   const setDimensions = () => {
-    const maxSide = 400
+    const maxSide = 200
+    const gap = 80
     let ratio = 0
     let width = image ? image.width : maxSide
     let height = image ? image.height : maxSide
+    let newX = (window.innerWidth - (maxSide * 4)) / 2
+    let newY = 0
 
     if (width > maxSide) {
       ratio = maxSide / width
@@ -26,6 +30,51 @@ const URLImage = ({ data, onClick, idx }, ref) => {
     }
 
     cache[`${idx}`] = { width, height}
+
+    // if (idx % 3 !== 0) {
+    //   newY = (prevHeight * ratio) + (gap * (idx % 3))
+    //   setY(newY)
+    //   console.log(idx, newY, cache[idx - 1])
+    // }
+
+    switch (idx) {
+      case 0:
+      case 1:
+      case 2:
+      case 3:
+        newX = ((window.innerWidth - (maxSide * 5 + gap * 4)) / 2)
+        setX(newX)
+        break;
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+        newX = ((window.innerWidth - (maxSide * 5 + gap * 4)) / 2) + ((maxSide + gap) * 1)
+        setX(newX)
+        break;
+      case 8:
+      case 9:
+      case 10:
+      case 11:
+        newX = ((window.innerWidth - (maxSide * 5 + gap * 4)) / 2) + ((maxSide + gap) * 2)
+        setX(newX)
+        break;
+      case 12:
+      case 13:
+      case 14:
+      case 15:
+        newX = ((window.innerWidth - (maxSide * 5 + gap * 4)) / 2) + ((maxSide + gap) * 3)
+        setX(newX)
+        break;
+      case 16:
+      case 17:
+      case 18:
+        newX = ((window.innerWidth - (maxSide * 5 + gap * 4)) / 2) + ((maxSide + gap) * 4)
+        setX(newX)
+        break;
+      default:
+
+    }
 
     setWidth(width)
     setHeight(height)
